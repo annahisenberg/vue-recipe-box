7661ac81b482262ad6fb262477299dde8d699b63
Annah Isenberg <annah.isenberg@msionline.com>
2019-04-10 08:31:03 -0500

    add a v-if on the recipe container

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -1,34 +1,39 @@
 <template>
   <b-container>
-    <b-card bg-variant="info" text-variant="white" id="recipe-container">
+    <b-modal id="modal-1">
+      <b-form-group label="title" label-for="input-1" variant="primary">
+        <b-form-input id="input-1" v-model="form.title" type="text" placeholder="title"></b-form-input>
+      </b-form-group>
+      <b-form-group label="ingredients:">
+        <b-form-input
+          @change="separateIngredients"
+          v-model="form.Ingredients"
+          type="text"
+          placeholder="ingredients"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group label="directions:">
+        <b-form-input
+          @change="separateDirections"
+          v-model="form.Directions"
+          type="text"
+          placeholder="directions"
+        ></b-form-input>
+      </b-form-group>
+      <div slot="modal-footer">
+        <b-button @click.prevent="saveNewRecipe">Add Recipe</b-button>
+      </div>
+    </b-modal>
+    <b-card
+      bg-variant="info"
+      text-variant="white"
+      id="recipe-container"
+      v-if="Object.keys(currentRecipe).length > 0"
+    >
       <b-button-group>
         <b-button v-b-modal.modal-1>+</b-button>
         <b-button @click="deleteRecipe">Delete Recipe</b-button>
       </b-button-group>
-      <b-modal id="modal-1">
-        <b-form-group label="title" label-for="input-1" variant="primary">
-          <b-form-input id="input-1" v-model="form.title" type="text" placeholder="title"></b-form-input>
-        </b-form-group>
-        <b-form-group label="ingredients:">
-          <b-form-input
-            @change="separateIngredients"
-            v-model="form.Ingredients"
-            type="text"
-            placeholder="ingredients"
-          ></b-form-input>
-        </b-form-group>
-        <b-form-group label="directions:">
-          <b-form-input
-            @change="separateDirections"
-            v-model="form.Directions"
-            type="text"
-            placeholder="directions"
-          ></b-form-input>
-        </b-form-group>
-        <div slot="modal-footer">
-          <b-button @click.prevent="saveNewRecipe">Add Recipe</b-button>
-        </div>
-      </b-modal>
       <b-card-text class="m-2">
         <h2>{{ currentTitle }}</h2>
         <div id="recipe-container">
